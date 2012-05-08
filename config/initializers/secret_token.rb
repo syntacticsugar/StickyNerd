efd177570a3ffef7f83ba62f356ddd2c07ea99e8
RomyRomy <romy@romyromy.com>
2012-05-08 14:40:23 -0400

    i am the stickiest nerd in the world.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stickynerd::Application.config.secret_token = 'ee066d14699e698f7446df1d9d00708515e80e6511e7392412a4e3853b45661df8c86c64ca4e858f8f599b04ee95a4b13a73ae0f6ffee1e11829aaa02eaf0e8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Stickynerd::Application.config.session_store :cookie_store, key: '_stickynerd_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Stickynerd::Application.config.session_store :active_record_store
